5b2027589120f7449a8087edebbefdea532ab6f7
Imène ALLOUCJE <https://github.com/Imeneallouche>
2022-09-10 20:01:58 +0100

    script updated => scroll to top setting grid => version 1.2

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const settingGrid = document.querySelector(".Setting");
 const checkboxes = Array.from(
   document.querySelectorAll(".Grid input[type='checkbox']")
 );
+const settingButton = document.querySelector(".Setting button");
+const levelButtons = Array.from(
+  document.querySelectorAll(".Setting input[type='button']")
+);
 
+let play = false;
 /*
 
 
@@ -25,11 +30,24 @@ function makeSquares(element) {
 }
 
 //function to make the setting grid at the level of the game grid
-function settingOn() {
+function SettingOn() {
   settingGrid.style.position = "relative";
   settingGrid.style.top = `${-settingGrid.offsetHeight}px`;
 }
 
+//function to fix setting button's place
+function SettingButtonFix() {
+  settingButton.style.position = "relative";
+  settingButton.style.top = `${-settingButton.offsetHeight / 2}px`;
+  settingButton.style.left = `${-settingButton.offsetWidth}px`;
+}
+
+//function that makes the setting grid go to the top
+function settingOff(eventSelected) {
+  console.log(`Up now! because of ${eventSelected.target.value} button`);
+  settingGrid.style.position = "relative";
+  settingGrid.style.top = `${-3 * settingGrid.offsetHeight}px`;
+}
 /*
 
 
@@ -53,7 +71,19 @@ EVENT LISTENERS*/
     makeSquares(settingGrid);
     checkboxes.forEach((checkbox) => makeSquares(checkbox));
 
+    //fix the place of the setting button
+    SettingButtonFix();
+
     //make settings grid at same level as game grid
-    settingOn();
+    if (!play) {
+      SettingOn();
+    }
   });
 });
+
+levelButtons.forEach((levelButton) =>
+  levelButton.addEventListener("click", (eventSelected) => {
+    play = true;
+    settingOff(eventSelected);
+  })
+);
